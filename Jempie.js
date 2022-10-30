25119beb527fc13188588d3be9b495c860807eed
McJempers <116578231+McJempers@users.noreply.github.com>
2022-10-30 16:19:55 +0100

    still going

--- Jempie.js
+++ Jempie.js
@@ -4,7 +4,7 @@
  */
 window.onload = function() {
   // Month Day, Year Hour:Minute:Second, id-of-element-container
-  countUpFromTime("Oct 28, 2022 20:45:45", 'countup1'); // ****** Change this line!
+  countUpFromTime("Oct 30, 2022 16:20:00", 'countup1'); // ****** Change this line!
 };
 function countUpFromTime(countFrom, id) {
   countFrom = new Date(countFrom).getTime();
