aafca5a6f75aca23ce17dcc341fc4d23bddedd91
lla014 <116580676+lla014@users.noreply.github.com>
2023-11-07 12:37:53 +0100

    Update Jempie.js

--- Jempie.js
+++ Jempie.js
@@ -4,7 +4,7 @@
  */
 window.onload = function() {
   // Month Day, Year Hour:Minute:Second, id-of-element-container
-  countUpFromTime("Nov 4, 2023 23:00:00", 'countup1'); // ****** Change this line!
+  countUpFromTime("Nov 5, 2023 24:00:00", 'countup1'); // ****** Change this line!
 };
 function countUpFromTime(countFrom, id) {
   countFrom = new Date(countFrom).getTime();
