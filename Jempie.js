cc7b53ee0ddd514a1b25cb6b63536775d53b9e79
McJempers <116578231+McJempers@users.noreply.github.com>
2024-04-29 19:41:38 +0200

    Update Jempie.js

--- Jempie.js
+++ Jempie.js
@@ -4,7 +4,7 @@
  */
 window.onload = function() {
   // Month Day, Year Hour:Minute:Second, id-of-element-container
-  countUpFromTime("feb 17, 2024 22:00:00", 'countup1'); // ****** Change this line!
+  countUpFromTime("Apr 28, 2024 8:00:00", 'countup1'); // ****** Change this line!
 };
 function countUpFromTime(countFrom, id) {
   countFrom = new Date(countFrom).getTime();
